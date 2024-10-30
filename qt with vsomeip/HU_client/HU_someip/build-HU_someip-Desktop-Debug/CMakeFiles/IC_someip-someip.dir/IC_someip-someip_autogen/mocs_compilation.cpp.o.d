b047d33e27cd778b75053d1f337aecc10c80f90b
YEOHANBIN <113481955+YEOHANBIN@users.noreply.github.com>
2024-10-30 20:45:55 +0100

    Delete unrelated directories

--- qt with vsomeip/HU_client/HU_someip/build-HU_someip-Desktop-Debug/CMakeFiles/IC_someip-someip.dir/IC_someip-someip_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/IC_someip-someip.dir/IC_someip-someip_autogen/mocs_compilation.cpp.o: \
- /home/seame/Head-Unit-Team1/qt\ with\ vsomeip/HU_client/HU_someip/build-HU_someip-Desktop-Debug/IC_someip-someip_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- qt with vsomeip/HU_client/HU_someip/build-HU_someip-Desktop-Debug/CMakeFiles/IC_someip.dir/IC_someip_autogen/EWIEGA46WW/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/IC_someip.dir/IC_someip_autogen/EWIEGA46WW/qrc_qml.cpp.o: \
- /home/seame/Head-Unit-Team1/qt\ with\ vsomeip/HU_client/HU_someip/build-HU_someip-Desktop-Debug/IC_someip_autogen/EWIEGA46WW/qrc_qml.cpp \
- /usr/include/stdc-predef.h
