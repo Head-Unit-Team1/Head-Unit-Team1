422783893d3db7f4ba998f8e3c296ca64a1ce7f7
KunhoPark-Jason <l17021323@gmail.com>
2024-11-12 18:11:25 +0100

    Rotate video

--- yocto/meta-splash/recipes-core/files/playvideo.sh
+++ yocto/meta-splash/recipes-core/files/playvideo.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 chmod 666 /dev/fb0
-cvlc --vout fb --no-osd --fullscreen --play-and-exit /home/root/videos/splashvideo_02.mp4
+cvlc --vout fb --no-osd --fullscreen --aspect-ratio=16:9 --video-align=center --video-filter=rotate --rotate-angle=90 --play-and-exit /home/root/videos/splashvideo_02.mp4
 exit 0
+
