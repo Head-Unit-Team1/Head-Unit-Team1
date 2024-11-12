a3cd2e3df230dc563910ab6056dd63a988982a2f
JunSpring <wjdwnsqja08@gmail.com>
2024-11-12 17:07:27 +0100

    Set env variables

--- yocto/meta-ui/recipes-hu/files/Head_Unit_app/hu.sh
+++ yocto/meta-ui/recipes-hu/files/Head_Unit_app/hu.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# LD_LIBRARY_PATH=/usr/lib/:/opt/IC_someip/build/
-# COMMONAPI_CONFIG=/opt/IC_someip/commonapi4someip.ini
-# VSOMEIP_CONFIGURATION=/opt/IC_someip/json/IC.json
-# VSOMEIP_APPLICATION_NAME=IC-service
+LD_LIBRARY_PATH=/usr/lib/:/opt/Head_Unit_app/build/
+COMMONAPI_CONFIG=/opt/Head_Unit_app/commonapi4someip.ini
+VSOMEIP_CONFIGURATION=/opt/Head_Unit_app/json/HU.json
+VSOMEIP_APPLICATION_NAME=Head_Unit_app
 
-# export LD_LIBRARY_PATH
-# export COMMONAPI_CONFIG
-# export VSOMEIP_CONFIGURATION
-# export VSOMEIP_APPLICATION_NAME
+export LD_LIBRARY_PATH
+export COMMONAPI_CONFIG
+export VSOMEIP_CONFIGURATION
+export VSOMEIP_APPLICATION_NAME
 
 /opt/Head_Unit_app/build/Head_Unit_app --no-sandbox
